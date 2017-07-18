fe46fd7b36c6075269a67217320e52e6ace4b1b6
DESKTOP-HFP81MQ\mochi <mochi_ahphop008@yahoo.co.jp>
2017-07-18 21:56:06 +0900

    タイトル画面の基盤を作製。 次の活動で、タイトル画面で何ができるかを決定して、遷移や他のUIを追加したい。

---
+++ Assets/Yuzuki/Script/TittleStart.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TittleStart : MonoBehaviour {
+
+	public void StartTitle () {
+		transform.parent.Find ("text").gameObject.GetComponent<TouchStart> ().DoNonactive ();
+	}
+}
