e8d0968b668b06accda5212475417453d2cacd4f
DESKTOP-HFP81MQ\mochi <mochi_ahphop008@yahoo.co.jp>
2017-08-25 19:32:57 +0900

    チュートリアル変更途中

---
+++ Assets/Yuzuki/Script/DrowLineSupport.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrowLineSupport : MonoBehaviour {
+
+	TutorialManager mng;
+
+	// Use this for initialization
+	void Start () {
+		mng = GameObject.Find ("TutorialCanvas").GetComponent <TutorialManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
